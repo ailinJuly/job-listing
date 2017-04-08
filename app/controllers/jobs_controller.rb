e5ab3e6122b13a2d848ff17d8e64c0454aad449f
jy <ailinjulygao@gmail.com>
2017-04-08 18:58:21 +0800

    validates and crud

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -15,7 +15,37 @@ class JobsController < ApplicationController
   def edit
     @job = Job.find(params[:id])
 
-    end
+  end
 
+  def create
+    @job = Job.new(job_params)
+    if @job.save
+    redirect_to jobs_path
+  else
+    render :new
 
+  end
+  end
+
+  def update
+    @job = Job.find(params[:id])
+  if  @job.update(job_params)
+
+    redirect_to jobs_path,notice: "Update Success"
+  else
+    render :edit
+  end
+  end
+
+  def destroy
+    @job = Job.find(params[:id])
+    @job.destroy
+    redirect_to jobs_path,alert: "Job delelted!"
+  end
+
+private
+
+   def job_params
+     params.require(:job).permit(:title, :description)
+   end
 end
