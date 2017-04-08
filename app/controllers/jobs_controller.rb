ff94a3c73f0c1efb29d6475e932d0afb3a29e493
jy <ailinjulygao@gmail.com>
2017-04-08 12:03:42 +0800

    bootstrap

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,21 @@
 class JobsController < ApplicationController
+   before_action :authenticate_user! , only: [:new]
   def index
    @jobs = Job.all
  end
+
+  def new
+  @job = Job.new
+  end
+
+  def show
+  @job = Job.find(params[:id])
+
+  end
+  def edit
+    @job = Job.find(params[:id])
+
+    end
+
+
 end
