ecd173e6933e213ff20c03ef45dc14635908da66
高中华 <gaozhonghua@gaozhonghuadeMacBook-Pro.local>
2017-11-19 22:03:47 +0800

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :admin do
-        resources :jobs
+        resources :jobs do
+          collection do
+            post :bulk_update
+          end
+        end
+
       end
-      
+
       resources :jobs
       root 'jobs#index'
 end
